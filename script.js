bc1c1fbbdf6f87a8ef5e4a6c9babce28d57c4cd0
Majdi-Aref <>
2023-05-10 20:24:12 +0000

    Get game and reset buttons and result elements into script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// Get game buttons, reset button, and result elements
+const buttons = document.querySelectorAll(".game-button");
+const resetButton = document.querySelector("#reset-button");
+const roundResultText = document.querySelector("#round-result-text");
+const playerScore = document.querySelector("#player-score");
+const computerScore = document.querySelector("#computer-score");
 
